da89313461ae28345f852a3de5b01d8a14f9e217
Anna <ansh00@mail.ru>
2025-02-04 00:19:47 +0300

    [feat][FE] add page recipes and detail

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.send_recipes, name='recipes'),
+    path('', views.page_recipes, name='page_recipes'),
+    path('send_recipes/', views.send_recipes, name='send_recipes'),
+    path('<int:recipe_id>/', views.detail_recipes, name='detail_recipes'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404
 from .models import Recipe, Ingredient
+from django.core.paginator import Paginator
+import nltk
+
 
 def send_recipes(request):
     recipes = Recipe.objects.order_by('name')
     ingredients = Ingredient.objects.all()
-    return render(request, 'recipes/send_recipes.html', {'recipes':recipes, 'ingredients':ingredients})
+    return render(request, 'recipes/send_recipes.html', {'recipes': recipes, 'ingredients': ingredients})
+
+
+def page_recipes(request):
+    recipes_list = Recipe.objects.order_by('name')
+    paginator = Paginator(recipes_list, 12)
+    page_number = request.GET.get('page')
+    recipes = paginator.get_page(page_number)
+    return render(request, 'recipes/page_recipes.html', {'recipes': recipes})
+
+
+def detail_recipes(request, recipe_id):
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+    steps = nltk.sent_tokenize(recipe.instruction, language='russian')
+    ingredients = Ingredient.objects.filter(recipe_id=recipe_id)
+    return render(request, 'recipes/detail_recipes.html', {'recipe': recipe,
+                                                           'ingredients': ingredients, 'steps': steps}, )
