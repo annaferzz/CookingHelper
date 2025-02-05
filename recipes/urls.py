3ed319fb8b56e42dd988af335c7288e349698046
Daniya <baybikova.daniya@mail.ru>
2025-02-05 20:16:07 +0300

    create page shopping_list

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.page_recipes, name='page_recipes'),
     path('create_recipes/', views.create_recipes, name='create_recipes'),
-    path('send_recipes/', views.send_recipes, name='send_recipes'),
     path('<int:recipe_id>/', views.detail_recipes, name='detail_recipes'),
+    path('shopping/<int:recipe_id>/', views.shopping, name='shopping'),
+    path('shopping_list', views.shopping_list, name='shopping_list'),
 ]
